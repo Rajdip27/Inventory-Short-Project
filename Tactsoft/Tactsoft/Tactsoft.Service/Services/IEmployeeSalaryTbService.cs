5122ae3b6ac6669a9d2fb3006ca1bbc6ad591a89
Raj <srajdip920@gmail.com>
2023-02-09 01:25:41 +0600

    EmployeeSalary Index View Create

--- Tactsoft/Tactsoft/Tactsoft.Service/Services/IEmployeeSalaryTbService.cs
+++ Tactsoft/Tactsoft/Tactsoft.Service/Services/IEmployeeSalaryTbService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tactsoft.Core.Entities;
+using Tactsoft.Service.Services.Base;
 
 namespace Tactsoft.Service.Services
 {
-    internal interface IEmployeeSalaryTbService
+    public interface IEmployeeSalaryTbService:IBaseService<EmployeeSalaryTb>
     {
+
     }
 }
